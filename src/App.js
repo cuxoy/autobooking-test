5a5bf378b522fd4ced1671e32a5d13fca56419a3
Dmytro <ccccuxoy@gmail.com>
2022-07-14 19:52:36 +0300

    data fetched, options created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useMemo } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState({ services: [], brands: [], styles: [] });
+  const [loadingStatus, setLoadingStatus] = useState("loading");
+
+  useEffect(() => {
+    const requests = [
+      "https://autobooking.com/api/test/v1/search/terms",
+      "https://autobooking.com/api/test/v1/search/brands_terms",
+      "https://autobooking.com/api/test/v1/search/brands_terms",
+    ];
+    Promise.all(
+      requests.map((request) => {
+        return fetch(request).then((response) => response.json());
+      })
+    )
+      .then((res) =>
+        setData({ services: res[0], brands: res[1], styles: res[2] })
+      )
+      .then(() => setLoadingStatus("loaded"));
+  }, []);
+
+  console.log(data);
+
+  if (loadingStatus === "loading") {
+    return (
+      <div
+        className="loading"
+        style={{
+          "text-align": "center",
+          "font-size": "20px",
+          "margin-top": "40px",
+        }}
+      >
+        Loading...
+      </div>
+    );
+  } else if (loadingStatus === "loaded") {
+    return (
+      <div className="container">
+        <select name="services" id="services">
+          {data.services.data.map((item) => {
+            return <option value="">{item.label}</option>;
+          })}
+        </select>
+        <select name="brands" id="brands">
+          {data.brands.data.map((item) => {
+            return <option value="">{item.label}</option>;
+          })}
+        </select>
+        <select name="styles" id="styles">
+          {data.styles.data.map((item) => {
+            return <option value="">{item.label}</option>;
+          })}
+        </select>
+      </div>
+    );
+  } else {
+    return <div className="loading">Error</div>;
+  }
 }
 
 export default App;
